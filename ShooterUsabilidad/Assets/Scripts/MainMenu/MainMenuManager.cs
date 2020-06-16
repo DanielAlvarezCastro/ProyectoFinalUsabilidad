bfe911b915e8ac479af712adc30d5f9a05771740
Aramissss <gasardan@ucm.es>
2020-06-16 21:18:53 +0200

    Selección de armas(no funcional)

--- ShooterUsabilidad/Assets/Scripts/MainMenu/MainMenuManager.cs
+++ ShooterUsabilidad/Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -6,18 +6,38 @@ using TMPro;
 public class MainMenuManager : MonoBehaviour
 {
     public TextMeshProUGUI descriptionText;
+    public GameObject testSelection;
+    public GameObject weaponSelection;
+    int selectedTest;
+    int selectedWeapon;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ShowTestSelection();
     }
     public void UpdateDescription(string txt)
     {
         descriptionText.text = txt;
     }
-    public void GoToScene(int num)
+    public void ShowTestSelection()
     {
-        SceneManager.LoadScene(num);
+        testSelection.SetActive(true);
+        weaponSelection.SetActive(false);
+    }
+    public void ShowWeaponSelection()
+    {
+        testSelection.SetActive(false);
+        weaponSelection.SetActive(true);
+    }
+    public void SelectTest(int num)
+    {
+        selectedTest = num;
+        ShowWeaponSelection();
+    }
+    public void SelectWeapon(int num)
+    {
+        selectedWeapon = num;
+        SceneManager.LoadScene(selectedTest);
     }
     public void QuitGame()
     {
