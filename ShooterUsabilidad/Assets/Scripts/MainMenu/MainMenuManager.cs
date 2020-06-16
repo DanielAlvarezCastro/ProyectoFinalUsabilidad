3169aa5a36209c7a9ed4cae6afa2039f6d431418
Aramissss <gasardan@ucm.es>
2020-06-16 20:53:06 +0200

    Menú principal

---
+++ ShooterUsabilidad/Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+public class MainMenuManager : MonoBehaviour
+{
+    public TextMeshProUGUI descriptionText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void UpdateDescription(string txt)
+    {
+        descriptionText.text = txt;
+    }
+    public void GoToScene(int num)
+    {
+        SceneManager.LoadScene(num);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
