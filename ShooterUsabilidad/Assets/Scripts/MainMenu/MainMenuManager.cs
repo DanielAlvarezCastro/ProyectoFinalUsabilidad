9245eabb0f2d4df83565b9cd54c1529ec1cba208
Aramissss <gasardan@ucm.es>
2020-06-20 20:05:45 +0200

    Input de nombre en MainMenu

--- ShooterUsabilidad/Assets/Scripts/MainMenu/MainMenuManager.cs
+++ ShooterUsabilidad/Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -8,6 +8,9 @@ public class MainMenuManager : MonoBehaviour
     public TextMeshProUGUI descriptionText;
     public GameObject testSelection;
     public GameObject weaponSelection;
+    public GameObject nameSelection;
+    public TMP_InputField nameInput;
+    string selectedName;
     int selectedTest;
     int selectedWeapon;
     // Start is called before the first frame update
@@ -23,11 +26,19 @@ public class MainMenuManager : MonoBehaviour
     {
         testSelection.SetActive(true);
         weaponSelection.SetActive(false);
+        nameSelection.SetActive(false);
     }
     public void ShowWeaponSelection()
     {
         testSelection.SetActive(false);
         weaponSelection.SetActive(true);
+        nameSelection.SetActive(false);
+    }
+    public void ShowNameSelection()
+    {
+        testSelection.SetActive(false);
+        weaponSelection.SetActive(false);
+        nameSelection.SetActive(true);
     }
     public void SelectTest(int num)
     {
@@ -37,6 +48,13 @@ public class MainMenuManager : MonoBehaviour
     public void SelectWeapon(int num)
     {
         selectedWeapon = num;
+        ShowNameSelection();
+    }
+    public void StartGame()
+    {
+        if(nameInput.text=="")
+            nameInput.text = "ANON";
+        selectedName = nameInput.text;
         SceneManager.LoadScene(selectedTest);
     }
     public void QuitGame()
